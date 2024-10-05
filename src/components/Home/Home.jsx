ec4f53b30900586a8054a561c2fd4c0cd35df100
NOZPEN\shwhd <nicenoz>
2024-10-06 00:24:26 +0900

    test deploy

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi noz!{" "}
+                Hi niceNOZ!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
