9be808e3ca43be42febeaddd3fe2af2f0d0729ca
NOZPEN\shwhd <nicenoz>
2024-10-06 09:20:55 +0900

    react-pdf test update

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -6,13 +6,7 @@ import pdf from "../../Assets/../Assets/NOZResume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import { PDFViewer } from '@react-pdf/renderer';
-
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.js",
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -23,8 +17,7 @@ function ResumeNew() {
 
   return (
     <div>
-      <>
-       <Container fluid className="resume-section">
+      <Container fluid className="resume-section">
         <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
@@ -39,11 +32,11 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <PDFViewer>
-            <Document file={pdf} className="d-flex justify-content-center" >
-              <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-            </Document>
-          </PDFViewer>
+          <Document file={pdf} className="d-flex justify-content-center"
+          >
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} 
+            />
+          </Document>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
@@ -58,9 +51,9 @@ function ResumeNew() {
           </Button>
         </Row>
       </Container>
-      </>
     </div>
   );
 }
+
 
 export default ResumeNew;
