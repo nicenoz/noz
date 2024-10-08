a8eb93071ea39549bd3006868384c531283f117d
jhnoh <jhnoh@eduwill.net>
2024-10-08 14:16:03 +0900

    style update

--- src/components/About/AboutCard.jsx
+++ src/components/About/AboutCard.jsx
@@ -33,7 +33,7 @@ function AboutCard() {
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
+          <p style={{ color: "rgba(0, 128, 128, 1)" }}>
             "No Pain, No gain"{" "}
           </p>
           <footer className="blockquote-footer">Noz</footer>
